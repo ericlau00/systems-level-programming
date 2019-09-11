e5ba840abcfd3f10c0d6370eb5d98748b32fc95b
ericlau00 <ericlau41802@gmail.com>
2019-09-11 13:33:02 -0400

    solve 06

--- 06.c
+++ 06.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int sum_squares(int limit) {
+    int i;
+    int sum = 0;
+    for(i = 1; i < limit + 1; i++) {
+        sum += i * i;
+    }
+    return sum;
+}
+
+int square_sum(int limit) {
+    int i;
+    int sum = 0;
+    for(i = 1; i < limit + 1; i++) {
+        sum += i;
+    }
+    return sum * sum;
+}
+
+int difference(int limit) {
+    return square_sum(limit) - sum_squares(limit);
+}
+
 int main() {
+    printf("%d\n", difference(100));
     return 0;
 }
