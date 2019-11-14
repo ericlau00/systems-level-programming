f4982b7c58e1c36e476adcfbfec8e0d779faff9d
ericlau00 <ericlau41802@gmail.com>
2019-11-14 00:40:18 -0500

    finito?

--- main.c
+++ main.c
@@ -9,12 +9,31 @@
 
 int main() {
     struct stat buff; 
+    char * perms[8] = {"---","--x","-w-","-wx","r--","r-x","rw-","rwx"};
     if(stat("./main.c", &buff) < 0) {
         printf("errno: %d error: %s\n", errno, strerror(errno));
         return 0;
     }
-    printf("%ld bytes\n",buff.st_size);
-    printf("%o\n", buff.st_mode);
-    printf("%s", ctime(&buff.st_atime));
+    printf("metadata about ./main.c\n");
+    printf("\tbytes: %ld \n",buff.st_size);
+    int perm = buff.st_mode % 01000;
+    printf("\tpermissions: %o\n", perm);
+    printf("\tlast accessed: %s\n", ctime(&buff.st_atime));
+
+    printf("bytes humanized\n");
+    printf("\t%ld GB %ld MB %ld KB %ld B\n\n", 
+            buff.st_size/1000000000, 
+            buff.st_size/1000000, 
+            buff.st_size/1000, 
+            buff.st_size);
+
+    printf("ls -l form attempt\n");
+    printf("\t-%s%s%s %ld %s\n", 
+            perms[(perm % 01000) / 0100], 
+            perms[(perm % 0100) / 010], 
+            perms[perm % 010],
+            buff.st_size, 
+            ctime(&buff.st_atime));
+    // printf("%o\n", buff.st_mode % 01000);
     return 0;
 }
