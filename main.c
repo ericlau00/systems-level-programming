b316629f5eb67da257e82c4fb6ee2e75c51a65f3
ericlau00 <ericlau41802@gmail.com>
2019-11-15 11:45:01 -0500

    boiler plate

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h> //open 
+#include <unistd.h> //read, write, close 
+#include <errno.h> //errno
+#include <sys/stat.h> //umask, stat
+#include <time.h> //ctime
+
+int main() {
+    return 0;
+}
