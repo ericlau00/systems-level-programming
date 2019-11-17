9ec6f1348c082064fd85d18f369a1a1905bb5e58
ericlau00 <ericlau41802@gmail.com>
2019-11-17 00:22:51 -0500

    more

--- main.c
+++ main.c
@@ -12,11 +12,15 @@
 #include <pwd.h>
 #include <grp.h>
 
+int print_permissions(int perm);
+int print_time(char * time); 
+
 int main() {
-    char * perms[8] = {"---","--x","-w-","-wx","r--","r-x","rw-","rwx"};
     DIR * stream = opendir("./");
     struct dirent * file;
     long size = 0; 
+
+    printf("Statistics for directory: .\n");
     for(file = readdir(stream); file != NULL; file = readdir(stream)){
         struct stat buff;
         if(stat(file->d_name, &buff) < 0) {
@@ -26,30 +30,52 @@ int main() {
 
         if(file->d_type == DT_DIR) {
             printf("d");
-        } else {
+        } else { //files otherwise are regular files. 
             printf("-");
             size += buff.st_size;
         }
 
-        int perm = buff.st_mode % 01000;
+        print_permissions(buff.st_mode % 01000);
 
-        printf("%s%s%s", 
-                perms[(perm % 01000) / 0100], 
-                perms[(perm % 0100) / 010], 
-                perms[perm % 010]
-                );
+        printf(" %ld", buff.st_nlink);  
 
-        printf(" %ld", buff.st_nlink);
-        printf(" %s %s", getpwuid(buff.st_uid)->pw_name, getgrgid(buff.st_gid)->gr_name);
-        printf(" %5ld ", buff.st_size);
+        //2 left justifies 
+        //user and group owners 
+        printf(" %2s %s", getpwuid(buff.st_uid)->pw_name, getgrgid(buff.st_gid)->gr_name);
+        
+        //5 left justifies 
+        printf(" %5ld", buff.st_size);
 
-        char * time = ctime(&buff.st_atime);
-        time[strlen(time) - 1] = '\0';
-        printf("%s", time);
+        print_time(ctime(&buff.st_mtime));
+
         printf(" %s\n",file->d_name);
-
-
     }
     printf("total size: %ld bytes\n", size);
+    closedir(stream);
+    return 0;
+}
+
+int print_permissions(int perm) {
+    char * perms[8] = {"---","--x","-w-","-wx","r--","r-x","rw-","rwx"};
+    printf("%s%s%s", 
+        perms[(perm % 01000) / 0100], 
+        perms[(perm % 0100) / 010], 
+        perms[perm % 010]
+        );
+    return 0;
+}
+
+int print_time(char * time) {
+    //removes day of the week 
+    int strip_front;
+    for(strip_front = 0; strip_front < strlen(time); strip_front++) {
+        time[strip_front] = time[strip_front + 3];
+    }
+    //removes year 
+    int strip_end;
+    for(strip_end = 3; strip_end > 0; strip_end--) {
+        time[strlen(time) - strip_end] = '\0';
+    }
+    printf("%s", time);
     return 0;
 }
