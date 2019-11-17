92fad5342b84b15efc5efc3906f3c5be47dbfaa0
ericlau00 <ericlau41802@gmail.com>
2019-11-16 23:56:08 -0500

    more work

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,30 +9,45 @@
 #include <time.h> //ctime
 #include <dirent.h>
 #include <sys/types.h>
+#include <pwd.h>
+#include <grp.h>
 
 int main() {
+    char * perms[8] = {"---","--x","-w-","-wx","r--","r-x","rw-","rwx"};
     DIR * stream = opendir("./");
-    // printf("%p\n", stream);
-    struct dirent * entry;
+    struct dirent * file;
     long size = 0; 
-    for(entry = readdir(stream); entry != NULL; entry = readdir(stream)){
+    for(file = readdir(stream); file != NULL; file = readdir(stream)){
         struct stat buff;
-        if(stat(entry->d_name, &buff) < 0) {
+        if(stat(file->d_name, &buff) < 0) {
             printf("errno: %d error: %s\n", errno, strerror(errno));
             return 0;
         }
 
-        if(entry->d_type == 4) {
-            printf("directory");
+        if(file->d_type == DT_DIR) {
+            printf("d");
         } else {
-            printf("regular file");
+            printf("-");
+            size += buff.st_size;
         }
 
-        printf(" %ld bytes", buff.st_size);
+        int perm = buff.st_mode % 01000;
 
-        size += buff.st_size;
+        printf("%s%s%s", 
+                perms[(perm % 01000) / 0100], 
+                perms[(perm % 0100) / 010], 
+                perms[perm % 010]
+                );
 
-        printf(" %s\n",entry->d_name);
+        printf(" %ld", buff.st_nlink);
+        printf(" %s %s", getpwuid(buff.st_uid)->pw_name, getgrgid(buff.st_gid)->gr_name);
+        printf(" %5ld ", buff.st_size);
+
+        char * time = ctime(&buff.st_atime);
+        time[strlen(time) - 1] = '\0';
+        printf("%s", time);
+        printf(" %s\n",file->d_name);
+
 
     }
     printf("total size: %ld bytes\n", size);
