7f18d5c911a43c3389335890879d90086e42d903
Eric Lau <44459660+ericlau00@users.noreply.github.com>
2019-11-26 11:38:02 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h> //open 
+#include <unistd.h> //read, write, close 
+#include <errno.h> //errno
+#include <sys/stat.h> //umask, stat
+#include <time.h> //ctime
+#include <dirent.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <grp.h>
+
+int main() {
+    return 0;
+}
