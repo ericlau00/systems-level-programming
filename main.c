4ea6a881f1f774024982402a90e23ee1c842e173
ericlau00 <ericlau41802@gmail.com>
2019-11-27 01:02:09 -0500

    change includes

--- main.c
+++ main.c
@@ -1,16 +1,8 @@
-#define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <fcntl.h> //open 
-#include <unistd.h> //read, write, close 
-#include <errno.h> //errno
-#include <sys/stat.h> //umask, stat
-#include <time.h> //ctime
-#include <dirent.h>
-#include <sys/types.h>
-#include <pwd.h>
-#include <grp.h>
+#include <unistd.h>  
+
 
 char ** parse_args(char * line ) { 
     char * buff = line;
