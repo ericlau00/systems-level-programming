812d2c3844f2bf65826779c4cd45d681949ff4d9
ericlau00 <ericlau41802@gmail.com>
2020-01-19 05:40:52 -0500

    cleanup

--- client.c
+++ client.c
@@ -18,15 +18,9 @@ int main(int argc, char **argv) {
     int client_turn = 0;
 
     if (argc == 2) {
-        // char ** random_characters = malloc(sizeof(char *) * 16);
-        // get_random(random_characters);
 
-        // char random_characters[16][256] = {"hh", "hh", "hh", "hh", "hh", "hh", "hh", "hh", "hh", "hh", "hh", "hh", "hh", "hh", "hh", "hh" };
         char buffer[BUFFER_SIZE];
-        int i = 0;
-        // for(; i < 16; i++) {
-        //     printf("%s\n", random_characters[i]);
-        // }
+
         int listen_socket;
         listen_socket = client_listen();
 
@@ -56,9 +50,6 @@ int main(int argc, char **argv) {
 
                 fgets(res.content, sizeof(res.content), stdin);
                 *strchr(res.content, '\n') = '\0';
-                res.from_client = 0;
-                res.client_turn = 0;
-                res.prev_turn= 1;
                 write(client_1, &res, sizeof(res));
 
                 if(strcmp(temp, "Q") == 0) {
@@ -72,45 +63,17 @@ int main(int argc, char **argv) {
                         printf("[Guess]: ");
                         fgets(res.content, sizeof(res.content), stdin);
                         *strchr(res.content, '\n') = '\0';
-                        // res.from_client = 0;
-                        // res.client_turn = 0;
-                        // res.prev_turn= 1;
                         write(client_1, &res, sizeof(res));
                     }
                     if(strcmp(temp, "D") == 0) {
                         res.type = 3;
                         write(client_1, &res, sizeof(res));
                     }
-                    // printf("Enter name to eliminate // Press G to guess // Press D to finish:");
                 }
-
-                // printf("here\n");
-            // }
-
-            // while (read(client_1, buffer, sizeof(buffer))) {
-            //     printf("[other %d] received: [%s]\n", client_1, buffer);
-            //     write(client_1, buffer, sizeof(buffer));
-            // }
         }
     }
     else {
         client_0 = client_connect( TEST_IP );
-
-        // read all the characters
-        // char random_characters[16][256];
-        // read(client_0, &random_characters, sizeof(random_characters));
-
-        // int i = 0;
-        // for(; i < 16; i++) {
-        //     printf("%s\n", random_characters[i]);
-        // }
-
-        // struct response res;
-        // read(client_0, &res, sizeof(res));
-        // printf("%d\n", res.client_turn);
-        // printf("%d\n", res.prev_turn);
-        // printf("%s\n", res.content);
-
 
         while( 1 ) {
             struct response res;
@@ -165,41 +128,14 @@ int main(int argc, char **argv) {
                         printf("[Guess]: ");
                         fgets(res.content, sizeof(res.content), stdin);
                         *strchr(res.content, '\n') = '\0';
-                        // res.from_client = 0;
-                        // res.client_turn = 0;
-                        // res.prev_turn= 1;
                         write(client_0, &res, sizeof(res));
                     }
                     if(strcmp(temp, "D") == 0) {
                         res.type = 3;
                         write(client_0, &res, sizeof(res));
                     }
-                    // printf("Enter name to eliminate // Press G to guess // Press D to finish:");
                 }
             }
-            // printf("enter data: ");
-            // fgets(buffer, sizeof(buffer), stdin);
-            // *strchr(buffer, '\n') = 0;
-            // write(client_0, buffer, sizeof(buffer));
-            // printf("received: [%s]\n", message);
         }
     }
-
-
-    // int other_client_socket;
-    // char buffer[BUFFER_SIZE];
-
-    // if (argc == 2)
-    //     other_client_socket = client_connect( argv[1]);
-    // else
-    //     other_client_socket = client_connect( TEST_IP );
-
-    // while (1) {
-    //     printf("enter data: ");
-    //     fgets(buffer, sizeof(buffer), stdin);
-    //     *strchr(buffer, '\n') = 0;
-    //     write(other_client_socket, buffer, sizeof(buffer));
-    //     read(other_client_socket, buffer, sizeof(buffer));
-    //     printf("received: [%s]\n", buffer);
-    // }
 }
