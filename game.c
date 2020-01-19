9a7909c398b53a40d7d3e6fcf911c874eb5e86d3
ericlau00 <ericlau41802@gmail.com>
2020-01-19 14:36:20 -0500

    more refactor

--- game.c
+++ game.c
@@ -1,5 +1,43 @@
 #include "game.h"
 
+int game_logic(int client, struct response res, char * chosen) {
+    read(client, &res, sizeof(res));
+
+    if (res.type == QUESTION) {
+        answer_question(&res);
+    }
+    else if (res.type == GUESS) {
+        check_guess(&res, chosen, client);
+        ask_or_guess(&res);
+    }
+    else if (res.type == ANSWER) {
+        printf("Your opponent answered: %s\n", res.content);
+        printf("Enter characters name to flip or type done when finished: ");
+        char currflip[20];
+        fgets(currflip, sizeof(currflip), stdin);
+        *strchr(currflip, '\n') = 0;
+        while (strcmp(currflip, "done") != 0) {
+            //modify board
+            printf("You flipped: %s\n", currflip);
+            printf("Enter another characters name to flip or type done when finished: ");
+            fgets(currflip, sizeof(currflip), stdin);
+            *strchr(currflip, '\n') = 0;
+        }
+        res.type = 3;
+        strcpy(res.content, "flipping done");
+    }
+    else if (res.type == DONE) {
+        ask_or_guess(&res);
+    }
+    else if (res.type == GAMEOVER) {
+        printf("Congratulations u are slightly less trash than other guy\n");
+        exit(0);
+    }
+    write(client, &res, sizeof(res));
+
+    return 0;
+}
+
 int ask_initial_question(int client, struct response res) {
     res.type = 0;
     fgets(res.content, sizeof(res.content), stdin);
@@ -7,6 +45,30 @@ int ask_initial_question(int client, struct response res) {
     write(client, &res, sizeof(res));
 
     return 0;
+}
+
+int answer_question(struct response * res) {
+    printf("You were asked: %s\n", res->content);
+    printf("Your answer: ");
+    res->type = 2;
+    fgets(res->content, sizeof(res->content), stdin);
+    *strchr(res->content, '\n') = 0;
+
+    return 0;
+}
+
+int check_guess(struct response * res, char * chosen, int client) {
+    printf("Your opponent guessed: %s\n", res->content);
+    if (strcmp(res->content, chosen) == 0) {
+        printf("HAHAHAHAH U TRASH U LOST\n");
+        res->type = 4;
+        strcpy(res->content, "Game Over");
+        write(client, &res, sizeof(struct response));
+        exit(0);
+    }
+    else {
+        printf("The guess was incorrect\n");
+    }
 }
 
 int ask_or_guess(struct response * res) {
@@ -24,62 +86,6 @@ int ask_or_guess(struct response * res) {
         res->type = 0;
         strcpy(res->content, input);
     }
-}
-
-int game_logic(int client, struct response res, char * chosen) {
-    read(client, &res, sizeof(res));
-
-    // checking res type
-    // res = question
-    if (res.type == 0) {
-        printf("You were asked: %s\n", res.content);
-        printf("Your answer: ");
-        res.type = 2;
-        fgets(res.content, sizeof(res.content), stdin);
-        *strchr(res.content, '\n') = 0;
-    }
-    // res = guess
-    else if (res.type == 1) {
-        printf("Your opponent guessed: %s\n", res.content);
-        if (strcmp(res.content, chosen) == 0) {
-            printf("HAHAHAHAH U TRASH U LOST\n");
-            res.type = 4;
-            strcpy(res.content, "Game Over");
-            write(client, &res, sizeof(res));
-            exit(0);
-        }
-        else {
-            printf("The guess was incorrect\n");
-        }
-        ask_or_guess(&res);
-    }
-    // res = answer
-    else if (res.type == 2) {
-        printf("Your opponent answered: %s\n", res.content);
-        printf("Enter characters name to flip or type done when finished: ");
-        char currflip[20];
-        fgets(currflip, sizeof(currflip), stdin);
-        *strchr(currflip, '\n') = 0;
-        while (strcmp(currflip, "done") != 0) {
-            //modify board
-            printf("You flipped: %s\n", currflip);
-            printf("Enter another characters name to flip or type done when finished: ");
-            fgets(currflip, sizeof(currflip), stdin);
-            *strchr(currflip, '\n') = 0;
-        }
-        res.type = 3;
-        strcpy(res.content, "flipping done");
-    }
-    // res = done
-    else if (res.type == 3) {
-        ask_or_guess(&res);
-    }
-    // res = gameover
-    else if (res.type == 4) {
-        printf("Congratulations u are slightly less trash than other guy\n");
-        exit(0);
-    }
-    write(client, &res, sizeof(res));
 
     return 0;
 }
--- game.h
+++ game.h
@@ -1,6 +1,12 @@
 #include "networking.h"
 #include "random.h"
 
+#define QUESTION 0
+#define GUESS 1
+#define ANSWER 2
+#define DONE 3
+#define GAMEOVER 4
+
 struct response {
     int type; // 0 is question, 1 is guess, 2 is answer, 3 is done, 4 is gameover
     char content[256];
@@ -9,5 +15,9 @@ struct response {
 int game_logic(int client, struct response res, char * chosen);
 
 int ask_initial_question(int client, struct response res);
+
+int answer_question(struct response * res);
+
+int check_guess(struct response * res, char * chosen, int client);
 
 int ask_or_guess(struct response * res);
